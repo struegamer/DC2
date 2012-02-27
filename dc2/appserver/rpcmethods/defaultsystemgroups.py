1c1444c8f478379ed166a2710019bb615a3360b3
Stephan Adig <sh@sourcecode.de>
2012-02-27 13:15:30 +0100

    Fixed a bug in method names

--- dc2/appserver/rpcmethods/defaultsystemgroups.py
+++ dc2/appserver/rpcmethods/defaultsystemgroups.py
@@ -89,7 +89,7 @@ def dc2_configuration_systemgroups_update(rec=None):
     return xmlrpclib.fault(-32501,"Record could not be updated")
 
 @rpcmethod(name="dc2.configuration.systemgroups.delete",params={},returns={},is_xmlrpc=True,is_jsonrpc=True)
-def dc2_configuration_systemusers_delete(rec=None):
+def dc2_configuration_systemgroups_delete(rec=None):
     if rec is not None and type(rec) is types.DictType:
         if rec.has_key('_id'):
             response=tbl_systemgroups.remove(rec)
