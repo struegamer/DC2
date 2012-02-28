ca7e173025ab6e300dbafed817c12fce6cada7e9
Stephan Adig <sh@sourcecode.de>
2012-02-28 15:44:38 +0100

    Added license headers Added new yaml configurations Added new script dc2-create-nfsroot (NFY)

--- dc2/distrotools/actions/mirror.py
+++ dc2/distrotools/actions/mirror.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    (DC)² - DataCenter Deployment Control
+#    Copyright (C) 2010, 2011, 2012  Stephan Adig <sh@sourcecode.de>
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#################################################################################
+
 import sys
 import os
 import os.path
@@ -24,7 +44,7 @@ def do_mirror_job(default_config=None, distributions=None,distro_type=None):
         return None
     if distro_type in ["ubuntu","debian"]:
         do_debian_mirror_job(default_config,distributions,distro_type)
-    if distro_typE IN ["centos"]:
+    if distro_typE in ["centos"]:
         do_rsync_mirror_job(default_config,distributions,distro_type)
 
 
