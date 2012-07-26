0c447a484a6347ed5e875d4374cc3936459d0a08
Stephan Adig <sh@sourcecode.de>
2012-07-26 20:42:56 +0200

    Fixed import

--- dc2/lib/auth/kerberos/__init__.py
+++ dc2/lib/auth/kerberos/__init__.py
@@ -18,4 +18,3 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #################################################################################
 
-from xmlrpc import KerberosServerProxy
