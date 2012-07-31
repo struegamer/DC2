a3ad43ffdb90075f000539e372f773da25a21b9f
Stephan Adig <sh@sourcecode.de>
2012-07-31 10:36:59 -0700

    Adding property template_name

--- dc2/lib/web/pages/page.py
+++ dc2/lib/web/pages/page.py
@@ -44,6 +44,22 @@ class Page(object):
         self._pagedata={}
         self._page={}
 
+    #
+    # Properties
+    #
+
+    def _get_template_filename(self):
+        return self._template_name
+    def _set_template_filename(self,template_filename):
+        self._template_name=template_filename
+    def _del_template_filename(self):
+        del self._template_name
+    template_name=property(_get_template_filename,_set_template_filename,_del_template_filename,'Property: template_name')
+
+
+    # 
+    # public methods
+    #
     def add_page_data(self,data=None):
         if data is not None and type(data) is types.DictType:
             if data.has_key('context') or data.has_key('page'):
@@ -106,9 +122,6 @@ class Page(object):
                 return '%s/%s/edit' % (path_info,kwargs['id'])
             else:
                 return '%s/edit' % path_info
-
-    def create_action_url(self,action='',id=None,query_string=None):
-        pass
 
     def render(self):
         tmpl=self._tmpl_environ.get_template(self._template_name)
