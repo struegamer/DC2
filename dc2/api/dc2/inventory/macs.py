b5b284e1fb023c9f444507c21dc9e9590effd48d
Stephan Adig <sh@sourcecode.de>
2012-09-19 09:45:26 +0200

    return a list even when there is only one entry

--- dc2/api/dc2/inventory/macs.py
+++ dc2/api/dc2/inventory/macs.py
@@ -49,9 +49,7 @@ class Macs(RPCClient):
         if 'server_id' in kwargs:
             rec['server_id']=kwargs.get('server_id',None)
         macs=self._proxy.dc2.inventory.servers.macaddr.find(rec)
-        if len(macs)>0 and len(macs)==1:
-            return macs[0]
-        elif len(macs)>0:
+        if len(macs)>0:
             return macs
         return None
     def add(self,*args,**kwargs):
