e173dbafe42c9acdf25557018d7ff0fcad8c9713
Stephan Adig <sh@sourcecode.de>
2012-09-28 17:19:17 +0200

    FInalized ClassTempaltes controller and api

--- dc2/admincenter/apps/controllers/admin/backends_classtemplates.py
+++ dc2/admincenter/apps/controllers/admin/backends_classtemplates.py
@@ -93,7 +93,7 @@ class BackendClassTemplatesController(AdminController):
     def _init_backend(self,backend):
         self._transport=get_xmlrpc_transport(backend['backend_url'],backend['is_kerberos'])
         self._defaultclasses=DefaultClasses(self._transport)
-        self._classtempaltes=ClassTemplates(self._transport)
+        self._classtemplates=ClassTemplates(self._transport)
 
     @Logger
     @needs_auth
@@ -115,6 +115,130 @@ class BackendClassTemplatesController(AdminController):
         })
         page.set_action('index')
         result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'content':page.render()})
+        return result
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _new(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        page=self._prepare_page(verb)
+        backend_list=backends.backend_list()
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        defaultclasses=self._defaultclasses.list()
+        classtemplate=self._classtemplates.new()
+        page.set_title('DC2 Admindcenter - Backends - Class Templates - Add')
+        page.add_page_data({
+            'backendlist':backend_list,
+            'backend_id':backend_id,
+            'defaultclasses':defaultclasses,
+            'tmpl':classtemplate,
+        })
+        page.set_action('new')
+        result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'content':page.render()})
+        return result
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _edit(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        page=self._prepare_page(verb)
+        backend_list=backends.backend_list()
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        defaultclasses=self._defaultclasses.list()
+        classtemplate=self._classtemplates.get(id=verb['request_data']['id'])
+        if classtemplate['classes'] is not None:
+            for classe in classtemplate['classes']:
+                for k in xrange(len(defaultclasses)):
+                    if defaultclasses[k]['classname']==classe:
+                        a=defaultclasses[k]
+                        defaultclasses.remove(a)
+                        break;
+        else:
+            classtemplate['classes']=[]
+        web.debug('classtemplate: %s' % classtemplate)
+        page.set_title('DC2 Admindcenter - Backends - Class Templates - Edit')
+        page.add_page_data({
+            'backendlist':backend_list,
+            'backend_id':backend_id,
+            'defaultclasses':defaultclasses,
+            'tmpl':classtemplate,
+        })
+        page.set_action('edit')
+        result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'content':page.render()})
+        return result
+
+
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _create(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        result=json.loads(web.data())
+        classtemplate={}
+        classtemplate['name']=result['result']['classtemplate']['name']
+        classtemplate['description']=result['result']['classtemplate']['description']
+        classtemplate['classes']=result['result']['classtemplate']['classes']
+        self._classtemplates.add(classtemplate=classtemplate)
+        output_format=verb.get('request_output_format',None)
+        if output_format.lower()=='json':
+            result=self._prepare_output('json',verb['request_content_type'],verb['request_output_format'],{'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        else:
+            result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        return result
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _update(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        tmpl_id=verb['request_data']['id']
+        result=json.loads(web.data())
+        classtemplate={}
+        classtemplate['_id']=tmpl_id
+        classtemplate['name']=result['result']['classtemplate']['name']
+        classtemplate['description']=result['result']['classtemplate']['description']
+        classtemplate['classes']=result['result']['classtemplate']['classes']
+        self._classtemplates.update(classtemplate=classtemplate)
+        output_format=verb.get('request_output_format',None)
+        if output_format.lower()=='json':
+            result=self._prepare_output('json',verb['request_content_type'],verb['request_output_format'],{'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        else:
+            result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        return result
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _delete(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        tmpl_id=verb['request_data']['id']
+        result=self._classtemplates.delete(id=tmpl_id)
+        output_format=verb.get('request_output_format',None)
+        if output_format.lower()=='json':
+            result=self._prepare_output('json',verb['request_content_type'],verb['request_output_format'],{'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        else:
+            result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
         return result
 
 
