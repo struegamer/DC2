6c86367621fb66f6adfc462b87a14f8473d2fd00
Stephan Adig <sh@sourcecode.de>
2012-10-01 09:08:49 +0200

    Add new configuration class SysGroups

--- dc2/api/dc2/configuration/sysgroups.py
+++ dc2/api/dc2/configuration/sysgroups.py
@@ -18,8 +18,29 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #################################################################################
 
+from dc2.api import RPCClient
+import web
 
-from environments import Environments
-from defaultclasses import DefaultClasses
-from classtemplates import ClassTemplates
-from sysgroups import SysGroups
+class SysGroups(RPCClient):
+    def find(self, rec=None):
+        if rec is None:
+            grouplist=self._proxy.dc2.configuration.sysgroups.list()
+            return grouplist
+        if rec is not None:
+            if type(rec) is not types.DictType:
+                raise Exception('The search argument is not a dictionary')
+            grouplist=self._proxy.dc2.configuration.sysgroups.list(search)
+            return grouplist
+
+    def list(self):
+        grouplist=self.find()
+        return grouplist
+
+    def new(self):
+        rec={}
+        rec['groupname']=''
+        rec['gid']=''
+        rec['is_system_group']='0'
+        rec['is_admin_group']='0'
+        return rec
+
