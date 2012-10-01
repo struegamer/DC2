21575d384b54cf35fc1a14d035233d98a240c91b
Stephan Adig <sh@sourcecode.de>
2012-10-01 14:49:11 +0200

    Finalizing BackendSysUserController

--- dc2/admincenter/apps/controllers/admin/backends_sysusers.py
+++ dc2/admincenter/apps/controllers/admin/backends_sysusers.py
@@ -86,4 +86,150 @@ except ImportError,e:
 
 tmpl_env=Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 
+class BackendSysUserController(AdminController):
+    CONTROLLER_IDENT={'title':'DC2 Backends System Users','url':'/admin/backends/sysusers','show_in_menu':'False'}
+
+    def _init_backend(self,backend):
+        self._transport=get_xmlrpc_transport(backend['backend_url'],backend['is_kerberos'])
+        self._sysusers=SysUsers(self._transport)
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _index(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        page=self._prepare_page(verb)
+        backend_list=backends.backend_list()
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        userlist=self._sysusers.list()
+        page.set_title('DC2 Admincenter - Backends - System Users - Index')
+        page.add_page_data({
+            'backendlist':backend_list,
+            'backend_id':backend_id,
+            'backend_sysusers':userlist
+        })
+        page.set_action('index')
+        result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'content':page.render()})
+        return result
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _new(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        page=self._prepare_page(verb)
+        backend_list=backends.backend_list()
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        user=self._sysusers.new()
+        page.set_title('DC2 Admincenter - Backends - System Users - Add')
+        page.add_page_data({
+            'backendlist':backend_list,
+            'backend_id':backend_id,
+            'user':user
+        })
+        page.set_action('new')
+        result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'content':page.render()})
+        return result
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _edit(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        page=self._prepare_page(verb)
+        backend_list=backends.backend_list()
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        user=self._sysusers.get(id=verb['request_data']['id'])
+        page.set_title('DC2 Admincenter - Backends - System Users - Edit')
+        page.add_page_data({
+            'backendlist':backend_list,
+            'backend_id':backend_id,
+            'user':user
+        })
+        page.set_action('edit')
+        result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'content':page.render()})
+        return result
+
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _create(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        backend_list=backends.backend_list()
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        result=json.loads(web.data())
+        rec=result['result']['user']
+        if 'is_admin' in rec:
+            rec['is_admin']=str(rec['is_admin'])
+        else:
+            rec['is_admin']='0'
+        self._sysusers.add(user=rec)
+        output_format=verb.get('request_output_format')
+        if output_format.lower()=='json':
+            result=self._prepare_output('json',verb['request_content_type'],verb['request_output_format'],{'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        else:
+            result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        return result
+
+       
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _update(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        backend_list=backends.backend_list()
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        result=json.loads(web.data())
+        rec=result['result']['user']
+        if 'is_admin' in rec:
+            rec['is_admin']=str(rec['is_admin'])
+        else:
+            rec['is_admin']='0'
+        self._sysusers.update(user=rec)
+        output_format=verb.get('request_output_format')
+        if output_format.lower()=='json':
+            result=self._prepare_output('json',verb['request_content_type'],verb['request_output_format'],{'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        else:
+            result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        return result
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _delete(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        request_data=verb.get('request_data',None)
+        backend_id=params.get('backend_id',None)
+        backend=backends.backend_get({'_id':backend_id})
+        self._init_backend(backend)
+        if request_data is not None and request_data.get('id',None) is not None:
+            self._sysusers.delete(id=request_data.get('id',None))
+        output_format=verb.get('request_output_format',None)
+        if output_format is not None and output_format.lower()=='json':
+            result=self._prepare_output('json',verb['request_content_type'],verb['request_output_format'],{'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        else:
+            result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'redirect':{'url':'%s?backend_id=%s' % (self._controller_path,backend_id),'absolute':'true'}})
+        return result
+
+
+
+
+
 
