aa34bd0b6d6c58156febecbb011c2fb7e79c2ec0
Stephan Adig <sh@sourcecode.de>
2012-10-04 20:01:44 +0200

    Finalized controller

--- dc2/admincenter/apps/controllers/admin/installmethod.py
+++ dc2/admincenter/apps/controllers/admin/installmethod.py
@@ -122,15 +122,66 @@ class AdminInstallMethodController(AdminController):
         verb=kwargs.get('verb',None)
         backend_list=backends.backend_list()
         install_method=installmethods.installmethod_get({'_id':verb['request_data']['id']})
+        web.debug('INSTALL METHOD EDIT: %s' % install_method)
         page=self._prepare_page(verb)
-        page.set_title('DC2 Admincenter - Installmethod Types - New')
+        page.set_title('DC2 Admincenter - Installmethod Types - Edit')
         page.add_page_data({
             'backendlist':backend_list,
             'installmethod':install_method
         })
-        page.set_action('new')
+        page.set_action('edit')
         result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'content':page.render()})
         return result
 
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _create(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        install_method={}
+        install_method['type']=params.type
+        install_method['name']=params.name
+        installmethods.installmethod_add(install_method)
+        output_format=verb.get('request_output_format')
+        if output_format.lower()=='json':
+            result=self._prepare_output('json',verb['request_content_type'],verb['request_output_format'],{'redirect':{'url':self._controller_path,'absolute':'true'}})
+        else:
+            result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'redirect':{'url':self._controller_path,'absolute':'true'}})
+        return result
 
-    
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _update(self, *args, **kwargs):
+        params=web.input()
+        verb=kwargs.get('verb',None)
+        install_method={}
+        install_method['_id']=params._id
+        install_method['type']=params.type
+        install_method['name']=params.name
+        installmethods.installmethod_update(install_method)
+        output_format=verb.get('request_output_format')
+        if output_format.lower()=='json':
+            result=self._prepare_output('json',verb['request_content_type'],verb['request_output_format'],{'redirect':{'url':self._controller_path,'absolute':'true'}})
+        else:
+            result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'redirect':{'url':self._controller_path,'absolute':'true'}})
+        return result
+
+
+    @Logger
+    @needs_auth
+    @needs_admin
+    def _delete(self, *args, **kwargs):
+        verb=kwargs.get('verb',None)
+        request_data=verb.get('request_data',None)
+        if request_data is not None and request_data.get('id',None) is not None:
+            install_method={'_id':request_data.get('id',None)}
+            installmethods.installmethod_delete(install_method)
+        output_format=verb.get('request_output_format',None)
+        if output_format is not None and output_format.lower()=='json':
+            result=self._prepare_output('json',verb['request_content_type'],verb['request_output_format'],{'redirect':{'url':self._controller_path,'absolute':'true'}})
+        else:
+            result=self._prepare_output(verb['request_type'],verb['request_content_type'],output={'redirect':{'url':self._controller_path,'absolute':'true'}})
+        return result
+
