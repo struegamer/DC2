ee6e41395244d0ab4aafd63e23cc6640e47caee0
Stephan Adig <sh@sourcecode.de>
2012-10-17 12:08:07 +0200

    Adding Proteus APIObjects
    - Zone
    - HostRecord
    - Configuration

--- dc2/lib/ipam/proteus/objects/apientity.py
+++ dc2/lib/ipam/proteus/objects/apientity.py
@@ -26,13 +26,21 @@ class APIObject(object):
         obj_type=None
         if 'TypeRecord' in kwargs:
             _apientity=kwargs.get('TypeRecord',None)
+            if len(_apientity)==0:
+                return None
             obj_type=_apientity.get('type',None)
             del kwargs['TypeRecord']
         kwargs['data']=_apientity
         if obj_type is not None:
             if obj_type.lower()=='zone':
                 return Zone(*args, **kwargs)
-        return super(APIObject,cls).__new__(cls,*args, **kwargs)
+            if obj_type.lower()=='configuration':
+                return Configuration(*args, **kwargs)
+            if obj_type.lower()=='view':
+                return View(*args, **kwargs)
+            if obj_type.lower()=='hostrecord':
+                return HostRecord(*args, **kwargs)
+        return None
 
     def __init__(self, *args, **kwargs):
         print self.__class__.__name__
@@ -48,11 +56,15 @@ class ProteusPropertyObject(object):
         if self._property_string is None:
             return None
         property_list=self._property_string[:-1].split('|')
+        self.__dict__['_property_list']=[]
         for i in property_list:
-            print i
             arr=iter(i.split('='))
             dc=dict(zip(arr,arr))
+            self.__dict__['_property_list'].append(i.split('=')[0])
             self.__dict__.update(dc)
+    def __repr__(self):
+        a=super(ProteusPropertyObject,self).__repr__()
+        return '%s | Property List: %s' % (a,self._property_list)
 
 class ProteusDataObjects(object):
     def __init__(self, *args, **kwargs):
@@ -60,14 +72,23 @@ class ProteusDataObjects(object):
         if 'data' in kwargs:
             self._raw_data=kwargs.get('data',None)
         if self._raw_data is not None:
-            print self._raw_data['properties']
-            self._raw_data['properties']=ProteusPropertyObject(self._raw_data['properties'])
+            if 'properties' in self._raw_data:
+                self._raw_data['properties']=ProteusPropertyObject(self._raw_data['properties'])
     def __getattr__(self, name):
         if name in self.__dict__['_raw_data']:
             return self._raw_data[name]
         return super(ProteusDataObjects,self).__getattr__(name)
-
+    def __repr__(self):
+        a=super(ProteusDataObjects,self).__repr__()
+        return '%s | Members: %s' % (a,self._raw_data.keys())
 class Zone(ProteusDataObjects):
-    def __init__(self, *args, **kwargs):
-        super(Zone,self).__init__(*args, **kwargs)
-    
+    pass
+
+class Configuration(ProteusDataObjects):
+    pass
+
+class View(ProteusDataObjects):
+    pass
+
+class HostRecord(ProteusDataObjects):
+    pass
