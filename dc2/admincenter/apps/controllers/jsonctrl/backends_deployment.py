d178143fa3ce53eeedb3dce5af1dd34d6978a95e
Stephan Adig <sh@sourcecode.de>
2012-10-22 22:13:24 +0200

    PEP8 and Source Code Style

--- dc2/admincenter/apps/controllers/jsonctrl/backends_deployment.py
+++ dc2/admincenter/apps/controllers/jsonctrl/backends_deployment.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#################################################################################
+###############################################################################
 #
 #    (DC)² - DataCenter Deployment Control
 #    Copyright (C) 2010, 2011, 2012  Stephan Adig <sh@sourcecode.de>
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License along
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#################################################################################
+###############################################################################
 
 import sys
 import os
@@ -27,14 +27,14 @@ import json
 
 try:
     import web
-except ImportError,e:
+except ImportError, e:
     print "You need to install web.py"
     sys.exit(1)
 
 try:
     from dc2.lib.decorators import Logger
     from dc2.lib.transports import get_xmlrpc_transport
-except ImportError,e:
+except ImportError, e:
     print 'you do not have dc2.lib installed'
     print e
     sys.exit(1)
@@ -44,7 +44,7 @@ try:
     from dc2.admincenter.lib import backends
     from dc2.admincenter.lib.auth import needs_auth
     from dc2.admincenter.globals import logger
-except ImportError,e:
+except ImportError, e:
     print 'you have a problem with dc2.admincenter'
     print e
     sys.exit(1)
@@ -54,35 +54,35 @@ try:
     from dc2.api.dc2.inventory import Hosts
     from dc2.api.dc2.deployment import InstallState
 
-except ImportError,e:
+except ImportError, e:
     print 'you didn\'t have dc2.api installed'
     print e
     sys.exit(1)
 
 class JSONDeploymentBackendController(JSONController):
     def __init__(self, *args, **kwargs):
-        super(JSONDeploymentBackendController,self).__init__(*args,**kwargs)
+        super(JSONDeploymentBackendController, self).__init__(*args, **kwargs)
         self._prepare_urls()
 
     def _prepare_urls(self):
-        self.add_url_handler_to_verb('GET','backend_deployment_list','backend_deployment_list')
-        self.add_process_method('backend_deployment_list',self._backend_deployment_list)
+        self.add_url_handler_to_verb('GET', 'backend_deployment_list', 'backend_deployment_list')
+        self.add_process_method('backend_deployment_list', self._backend_deployment_list)
 
     @needs_auth
     @Logger(logger=logger)
     def _backend_deployment_list(self, *args, **kwargs):
-        verb=kwargs.get('verb',None)
+        verb = kwargs.get('verb', None)
         if verb is not None:
-            params=web.input()
-            backend_id=params.get('backend_id',None)
+            params = web.input()
+            backend_id = params.get('backend_id', None)
             if backend_id is not None:
-                backend=backends.backend_get({'_id':backend_id})
-                transport=get_xmlrpc_transport(backend['backend_url'],backend['is_kerberos'])
-                s=InstallState(transport)
-                deploymentlist=s.list()
-                result=self._prepare_output(result={'backend_id':backend_id,'datalist':deploymentlist})
+                backend = backends.backend_get({'_id':backend_id})
+                transport = get_xmlrpc_transport(backend['backend_url'], backend['is_kerberos'])
+                s = InstallState(transport)
+                deploymentlist = s.list()
+                result = self._prepare_output(result={'backend_id':backend_id, 'datalist':deploymentlist})
                 return result
-        result=self._prepare_output(result={'backend_id':backend_id,'datalist':[]})
+        result = self._prepare_output(result={'backend_id':backend_id, 'datalist':[]})
         return result
 
 
