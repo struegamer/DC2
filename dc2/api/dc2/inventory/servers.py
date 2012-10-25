69cf3a53c82ea6aa0ada8d4a81e52caf9d4bf554
Stephan Adig <sh@sourcecode.de>
2012-10-25 16:22:43 +0200

    New method 'delete_complete'
    - Supporting the full xmlrpc api

--- dc2/api/dc2/inventory/servers.py
+++ dc2/api/dc2/inventory/servers.py
@@ -22,53 +22,65 @@ from dc2.api import RPCClient
 
 class Servers(RPCClient):
 
-    def find(self,rec=None):
+    def find(self, rec=None):
         if rec is None:
-            serverlist=self._proxy.dc2.inventory.servers.list()
+            serverlist = self._proxy.dc2.inventory.servers.list()
             return serverlist
         if rec is not None:
             if type(rec) is not types.DictType:
                 # TODO: Add Real Exception
                 raise Exception('The search argument is not a dictionary')
-            serverlist=self._proxy.dc2.inventory.servers.find(rec)
+            serverlist = self._proxy.dc2.inventory.servers.find(rec)
             return serverlist
 
     def list(self):
-        serverlist=self.find()
+        serverlist = self.find()
         return serverlist
 
     def count(self):
         # TODO: Add a rpc call to appserver for counting
-        serverlist=self.find()
+        serverlist = self.find()
         return len(serverlist)
 
-    def get(self,*args,**kwargs):
-        rec={}
+    def get(self, *args, **kwargs):
+        rec = {}
         if 'id' in kwargs:
-            rec['_id']=kwargs.get('id',None)
+            rec['_id'] = kwargs.get('id', None)
         if 'serial_no' in kwargs:
-            rec['serial_no']=kwargs.get('serial_no',None)
-        server=self._proxy.dc2.inventory.servers.find(rec)
-        if len(server)==1:
+            rec['serial_no'] = kwargs.get('serial_no', None)
+        server = self._proxy.dc2.inventory.servers.find(rec)
+        if len(server) == 1:
             return server[0]
         return []
-    def add(self,*args,**kwargs):
-        server_rec=kwargs.get('server',None)
+
+    def add(self, *args, **kwargs):
+        server_rec = kwargs.get('server', None)
         if server_rec is not None:
             return self._proxy.dc2.inventory.servers.add(server_rec)
         return False
-    def update(self,*args,**kwargs):
-        server_rec=kwargs.get('server',None)
+
+    def update(self, *args, **kwargs):
+        server_rec = kwargs.get('server', None)
         if server_rec is not None:
             return self._proxy.dc2.inventory.servers.update(server_rec)
         return false
-    def delete(self, *args,**kwargs):
-        server_rec={}
+
+    def delete(self, *args, **kwargs):
+        server_rec = {}
         if 'server_id' in kwargs:
-            server_rec['_id']=kwargs.get('server_id',None)
+            server_rec['_id'] = kwargs.get('server_id', None)
         if 'serial_no' in kwargs:
-            server_rec['serial_no']=kwargs.get('serial_no',None)
-        if len(server_rec)>0:
+            server_rec['serial_no'] = kwargs.get('serial_no', None)
+        if len(server_rec) > 0:
             return self._proxy.dc2.inventory.servers.delete(server_rec)
         return False
-            
+
+    def delete_complete(self, *args, **kwargs):
+        server_rec = {}
+        if 'server_id' in kwargs:
+            server_rec['_id'] = kwargs.get('server_id', None)
+        if 'serial_no' in kwargs:
+            server_rec['serial_no'] = kwargs.get('serial_no', None)
+        if len(server_rec) > 0:
+            return self._proxy.dc2.inventory.servers.delete_complete(server_rec)
+        return False
