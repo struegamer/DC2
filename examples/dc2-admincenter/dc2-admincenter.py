5b3e753ad6484df87ee0ce4885360c22f2775755
Stephan Adig <sh@sourcecode.de>
2012-11-02 13:03:10 +0100

    Fixing bug in is_admin hook

--- examples/dc2-admincenter/dc2-admincenter.py
+++ examples/dc2-admincenter/dc2-admincenter.py
@@ -42,7 +42,7 @@ try:
     from dc2.lib.db.mongo import MongoStore
     from dc2.lib.auth.helpers import get_realname
     from dc2.lib.auth.helpers import check_membership_in_group
-except ImportError,e:
+except ImportError, e:
     print "Your DC2 installation is not correct"
     print e
     sys.exit(1)
@@ -51,7 +51,7 @@ except ImportError,e:
 #
 try:
     from dc2.admincenter.apps import MainAppHandler
-except ImportError,e:
+except ImportError, e:
     print "You didn't install DC² correctly"
     print e
     sys.exit(1)
@@ -59,30 +59,32 @@ except ImportError,e:
 try:
     from settings import MONGOS
     from settings import GRP_NAME_DC2ADMINS
-except ImportError,e:
+except ImportError, e:
     print 'You do not have a settings file.'
     sys.exit(1)
 
 
 urls = (
     "(.*)", 'MainAppHandler',
-    #"/login","Login",
-    #'/admin',admin.app_admin
+    # "/login","Login",
+    # '/admin',admin.app_admin
     )
 
-session_db=MONGOS["admincenter"]["database"].get_db()
+session_db = MONGOS["admincenter"]["database"].get_db()
 
 app = web.application(urls, globals(), autoreload=True)
-sessions=web.session.Session(app,MongoStore(session_db,'sessions'))
+sessions = web.session.Session(app, MongoStore(session_db, 'sessions'))
 
 
 def session_hook():
-    web.ctx.session=sessions
+    web.ctx.session = sessions
 
 def is_admin():
     if 'authenticated' in web.ctx.session:
-        web.ctx.session.realname=get_realname(web.ctx.session.username)
-        web.ctx.session.is_dc2admin=check_membership_in_group(web.ctx.session.username,GRP_NAME_DC2ADMINS)
+        if web.ctx.session.authenticated:
+            web.ctx.session.realname = get_realname(web.ctx.session.username)
+            web.ctx.session.is_dc2admin = check_membership_in_group(web.ctx.session.username, GRP_NAME_DC2ADMINS)
+
 
 app.add_processor(web.loadhook(session_hook))
 app.add_processor(web.loadhook(is_admin))
