69a4c5d0e6fa7f55ead396d0e2e0308bff3feae7
Stephan Adig <sh@sourcecode.de>
2012-11-09 17:05:17 +0100

    import BackendSettings class

--- dc2/api/dc2/settings/__init__.py
+++ dc2/api/dc2/settings/__init__.py
@@ -18,4 +18,4 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #################################################################################
 
-
+from backend_settings import BackendSettings
