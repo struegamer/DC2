6ac4758de2053b2565665013c21036af64f7bbd4
Stephan Adig <sh@sourcecode.de>
2012-11-26 19:53:55 +0100

    Added new Exception Class
    - IPAHostAddError

--- dc2/lib/freeipa/lib/objects/exceptions.py
+++ dc2/lib/freeipa/lib/objects/exceptions.py
@@ -29,3 +29,11 @@ class IPAHostNotFound(Exception):
 
 
 
+class IPAHostAddError(Exception):
+    def __init__(self, error_code, error_message):
+        self._error_code = error_code
+        self._error_message = error_message
+
+    def __str__(self):
+        return 'IPAHostAddError: {0} - {1}'.format(self._error_code,
+                                                   self._error_message)
