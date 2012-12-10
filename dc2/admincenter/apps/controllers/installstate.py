8221bdb4a5cbc29cb9c04681133c2d2bd19d043e
Stephan Adig <sh@sourcecode.de>
2012-12-10 17:15:07 +0100

    Nother bugfix

--- dc2/admincenter/apps/controllers/installstate.py
+++ dc2/admincenter/apps/controllers/installstate.py
@@ -129,7 +129,7 @@ class InstallStateController(RESTController):
             except ImportError as e:
                 print('You did not install dc2.api')
                 print(e)
-                sys.exit(1)        
+                sys.exit(1)
         self._freeipa = None
         if FREEIPA_ENABLED:
             self._freeipa = FreeIPA_Hosts(self._transport)
@@ -201,14 +201,12 @@ class InstallStateController(RESTController):
         installstate_rec['status'] = installstate['status']
         self._installstate.update(rec=installstate_rec)
         backend_settings = self._backend_settings.get()
-        
-
-            if installstate['status'] == 'deploy':
-                host = self._hosts.get({'_id':installstate_rec['host_id']})
-                ipa_info = {'fqdn':'{0}.{1}'.format(host['hostname'], host['domainname']),
-                            'description':'Auto-Added from DC2',
-                            'random':True}
-                ipa_result = self._freeipa.add('{0}.{1}'.format(host['hostnmae'], host['domainname']), ipa_info)
+        if installstate['status'] == 'deploy':
+            host = self._hosts.get({'_id':installstate_rec['host_id']})
+            ipa_info = {'fqdn':'{0}.{1}'.format(host['hostname'], host['domainname']),
+                        'description':'Auto-Added from DC2',
+                        'random':True}
+            ipa_result = self._freeipa.add('{0}.{1}'.format(host['hostnmae'], host['domainname']), ipa_info)
         result = self._prepare_output('json', verb['request_content_type'], 'json', {'redirect':{'url':'%s/%s?backend_id=%s' % (self._controller_path, installstate['_id'], self._backend_id), 'absolute':'true'}})
         return result
 
