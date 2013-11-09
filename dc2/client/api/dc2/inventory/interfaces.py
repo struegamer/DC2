1452bfe0535098729ca7c8d99a0c856e2a0987b1
Stephan Adig <sh@sourcecode.de>
2013-11-09 13:54:42 +0100

    PEP-8 Changes

--- dc2/client/api/dc2/inventory/interfaces.py
+++ dc2/client/api/dc2/inventory/interfaces.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#################################################################################
+###############################################################################
 #
 #    (DC)² - DataCenter Deployment Control
 #    Copyright (C) 2010, 2011, 2012, 2013  Stephan Adig <sh@sourcecode.de>
@@ -16,19 +16,23 @@
 #    You should have received a copy of the GNU General Public License along
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#################################################################################
+###############################################################################
 
 import re
 import subprocess
 
+
 class Interfaces(object):
     def __init__(self):
         self._get_nics()
+
     def _get_nics(self):
-        ipconfig=subprocess.Popen(["ip","l"],stdout=subprocess.PIPE)
-        p=re.compile(r"\s+link/ether\s+([a-zA-Z0-9:]+)", re.VERBOSE)
-        self.__dict__["nics"]=[]
+        ipconfig = subprocess.Popen(
+            ["ip", "l"],
+            stdout=subprocess.PIPE)
+        p = re.compile(r"\s+link/ether\s+([a-zA-Z0-9:]+)", re.VERBOSE)
+        self.__dict__["nics"] = []
         for i in ipconfig.stdout:
-            found=p.search(i)
+            found = p.search(i)
             if found is not None:
                 self.__dict__["nics"].append(found.group(1))
