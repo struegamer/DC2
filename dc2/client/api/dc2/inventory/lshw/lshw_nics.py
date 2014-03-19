d4baf979fa1f9634c4719fa065b407adb9a58011
Stephan Adig <sh@sourcecode.de>
2014-03-19 20:31:19 +0100

    Fixed some logic bugs

--- dc2/client/api/dc2/inventory/lshw/lshw_nics.py
+++ dc2/client/api/dc2/inventory/lshw/lshw_nics.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 #
+# (DC)² - DataCenter Deployment Control
+# Copyright (C) 2010, 2011, 2012, 2013, 2014 Stephan Adig <sh@sourcecode.de>
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    (DC)² - DataCenter Deployment Control
-#    Copyright (C) 2010, 2011, 2012, 2013, 2014  Stephan Adig <sh@sourcecode.de>
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
 import sys
@@ -34,33 +33,36 @@ class LSHWNics(LSHWBase):
 
     def __init__(self):
         super(LSHWNics, self).__init__()
-        self._nics = {}
+        self._nics = []
+        self._find_nics()
 
     def _find_nics(self):
         find_nics = etree.XPath(".//node[@class='{0}']".format(
             self._NODE_CLASS_NAME))
+        _nics = {}
         for nic in find_nics(self._inventory):
             for nic_tag in nic:
                 if nic_tag.tag == 'configuration':
-                    self._nics['configuration'] = {}
+                    _nics['configuration'] = {}
                     for config in nic_tag:
                         config_settings = config.attrib
-                        for setting in config_settings:
-                            self._nics['configuration'][setting] =\
-                                config_settings[setting]
+                        _nics['configuration'][config_settings['id']] =\
+                            config_settings['value']
                 elif nic_tag.tag == 'capabilities':
-                    self._nics['capabilities'] = {}
+                    _nics['capabilities'] = {}
                     for cap in nic_tag:
                         cap_data = cap.attrib
-                        self._nics['capabilities'][cap_data['id']] = cap.text
+                        _nics['capabilities'][cap_data['id']] = cap.text
                 elif nic_tag.tag == 'resources':
-                    self._nics['resources'] = {}
+                    _nics['resources'] = {}
                     for resource in nic_tag:
                         resource_data = resource.attrib
-                        self._nics['resources'][resource_data['type']] =\
+                        _nics['resources'][resource_data['type']] =\
                             resource_data['value']
                 else:
-                    self._nics[nic_tag.tag] = nic.text
+                    _nics[nic_tag.tag] = nic_tag.text
+
+            self._nics.append(_nics)
 
     def _get_nics(self):
         return self._nics
