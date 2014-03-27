ef9beb1df25023af8447879f840cd6eead3402c2
Stephan Adig <sh@sourcecode.de>
2014-03-27 15:20:28 +0100

    PEP8 Changes and refactoring for Python3

--- components/dc2-admincenter/dc2/admincenter/globals/webconst.py
+++ components/dc2-admincenter/dc2/admincenter/globals/webconst.py
@@ -1,38 +1,37 @@
 # -*- coding: utf-8 -*-
-#################################################################################
 #
-#    (DC)² - DataCenter Deployment Control
-#    Copyright (C) 2010, 2011, 2012, 2013, 2014  Stephan Adig <sh@sourcecode.de>
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# (DC)² - DataCenter Deployment Control
+# Copyright (C) 2010, 2011, 2012, 2013, 2014 Stephan Adig <sh@sourcecode.de>
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#################################################################################
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 
 CSS_FILES = [
-        '/static/css/bootstrap/bootstrap.css',
-        '/static/css/admincenter/base.css',
-        '/static/css/bootstrap/bootstrap-responsive.css',
-        '/static/css/datatable/jquery.dataTables.css',
-        ]
+    '/static/css/bootstrap/bootstrap.css',
+    '/static/css/admincenter/base.css',
+    '/static/css/bootstrap/bootstrap-responsive.css',
+    '/static/css/datatable/jquery.dataTables.css',
+]
 
 JS_LIBS = [
-        '/static/js/jquery/jquery.js',
-        '/static/js/bootstrap/bootstrap.js',
-        '/static/js/datatable/jquery.dataTables.js',
-        '/static/js/datatable/DT_bootstrap.js',
-        '/static/js/jplugins/jquery.rightClick.js',
-        '/static/js/jplugins/jquery.formparams.js',
-        '/static/js/admincenter/utils.js',
-        '/static/js/admincenter/main.js',
-        ]
+    '/static/js/jquery/jquery.js',
+    '/static/js/bootstrap/bootstrap.js',
+    '/static/js/datatable/jquery.dataTables.js',
+    '/static/js/datatable/DT_bootstrap.js',
+    '/static/js/jplugins/jquery.rightClick.js',
+    '/static/js/jplugins/jquery.formparams.js',
+    '/static/js/admincenter/utils.js',
+    '/static/js/admincenter/main.js',
+]
 
