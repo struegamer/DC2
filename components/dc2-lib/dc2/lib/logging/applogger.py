efd4c859863f17e0649b16ba9384cd6d39af089c
Stephan Adig <sh@sourcecode.de>
2014-03-27 13:18:26 +0100

    PEP8 Changes

--- components/dc2-lib/dc2/lib/logging/applogger.py
+++ components/dc2-lib/dc2/lib/logging/applogger.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
-###############################################################################
 #
-#    (DC)² - DataCenter Deployment Control
-#    Copyright (C) 2010, 2011, 2012, 2013, 2014  Stephan Adig <sh@sourcecode.de>
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# (DC)² - DataCenter Deployment Control
+# Copyright (C) 2010, 2011, 2012, 2013, 2014 Stephan Adig <sh@sourcecode.de>
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-###############################################################################
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 
 import logging
 
 
 LOG_INFO = 1
 LOG_DEBUG = 10
+
 
 class AppLogger(object):
     def __init__(self, logger=None):
@@ -32,6 +32,7 @@ class AppLogger(object):
             self._logger = logger
 
     def _init_logging(self):
+        # FIXME: ASAP
         logger = logging.getLogger('DEFAULT_APP_LOGGER')
         logger.setLevel(LOGLEVEL)
 
